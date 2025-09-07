f5526fe3a04306fefb75df4222b29533f210042b
Valentino Terreno <ninot2016@gmail.com>
2025-09-07 11:51:18 -0300

    styles: gap en login logo

--- src/features/auth/auth-layout.tsx
+++ src/features/auth/auth-layout.tsx
@@ -9,7 +9,7 @@ export function AuthLayout({ children }: AuthLayoutProps) {
     <div className='container grid h-svh max-w-none items-center justify-center'>
       <div className='mx-auto flex w-full flex-col justify-center space-y-2 py-8 sm:w-[480px] sm:p-8'>
         <div className='mb-4 flex items-center justify-center'>
-          <Logo className='me-2' />
+          <Logo className='me-2 gap-3' />
         </div>
         {children}
       </div>
